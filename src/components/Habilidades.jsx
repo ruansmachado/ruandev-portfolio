38a5d7ed042167c21a69626f72921c794a2bdf43
Ruan Machado <ruansmachado@yahoo.com>
2025-08-29 00:43:26 -0300

    //

--- src/components/Habilidades.jsx
+++ src/components/Habilidades.jsx
@@ -1,21 +1,21 @@
 import { animation } from "../style";
 
 const Habilidades = ({ title, icon }) => (
-  <div
+  <article
     className={`flex flex-col mb-10 lg:px-8 py-6 lg:py-10 rounded-[10px] w-[8rem] lg:w-[10rem]  max-w-[370px] md:mr-10 mr-5 lg:mr-15 feedback-card ${animation.jumpIn}`}
   >
-    <div className="flex pb-1 justify-center">
-      <p className=" font-poppins font-normal text-[1rem] text-white ">
+    <header className="flex pb-1 justify-center">
+      <h3 className=" font-poppins font-normal text-[1rem] text-white ">
         {title}
-      </p>
-    </div>
+      </h3>
+    </header>
 
     <div className="flex flex-col">
       <div className="flex flex-row justify-center pt-5 mb-2">
-        <img src={icon} alt="icon" className="w-[63px] h-[60px] " />
+        <img src={icon} alt="" className="w-[63px] h-[60px] " />
       </div>
     </div>
-  </div>
+  </article>
 );
 
 export default Habilidades;
--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -2,29 +2,32 @@ import { github } from "../assets";
 import { projetos } from "../constants";
 
 const Projetos = ({ title, img, icon, link }) => (
-  <div className="flex justify-between flex-col lg:px-8 py-5 rounded-[20px]  max-w-[370px]  xs:mb-[5rem] feedback-card">
-    <div className="flex pb-5 justify-center">
-      <p className=" font-poppins font-normal text-[20px] leading-[32.4px] text-white ">
+  <article className="flex justify-between flex-col lg:px-8 py-5 rounded-[20px]  max-w-[370px]  xs:mb-[5rem] feedback-card">
+    <header className="flex pb-5 justify-center">
+      <h3 className=" font-poppins font-normal text-[20px] leading-[32.4px] text-white ">
         {title}
-      </p>
-    </div>
+      </h3>
+    </header>
 
     <div className="flex flex-col">
       <img
         src={img}
-        alt="icon"
+        alt={`Captura de tela do projeto: ${title}`}
         className="w-[20rem] h-[15rem] justify-center rounded"
       />
-      <div className="flex flex-row justify-center pt-5 ">
-        <img
-          src={icon}
-          alt="icon"
-          className="w-[43px] h-[40px] rounded-full cursor-pointer"
-          onClick={() => window.open(`${link}`)}
-        />
+      <div className="flex flex-row justify-center pt-5">
+        <a
+          href="link"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer hover:scale-110 transition-transform"
+          aria-label={`Visitar repositório do ${title} no GitHub`}
+        >
+          <img src={icon} alt="" className="w-[43px] h-[40px] rounded-full" />
+        </a>
       </div>
     </div>
-  </div>
+  </article>
 );
 
 export default Projetos;
